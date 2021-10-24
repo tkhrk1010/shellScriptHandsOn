2e26f9071c6e8b111c2f1933967c1f17262f507d
tkhrk <>
2021-10-24 16:49:01 +0900

    create python question

--- scripts/search.py
+++ scripts/search.py
@@ -8,13 +8,12 @@ bookSearchWord = input('Please enter book name you want to search.\n')
 
 print("Search results are below with search word: " + bookSearchWord)
 
-## to refer to paths regardless of the script location.
+# to refer to paths regardless of the script location.
 booksPath = os.path.join(os.path.dirname(__file__), '../db/books.csv')
+
+# Please implement below to behave same to shell script.
+# You can refer the document here:
 # https://docs.python.org/3/library/csv.html
-with open(booksPath, 'r') as csvfile:
-  reader = csv.DictReader(csvfile)
-  for row in reader:
-    bookName = row["book_name"]
-    area = row["area"]
-    if re.search(bookSearchWord, bookName):
-      print('Book name: '+ bookName +', Area: '+ area)
+# It is simple that it opens using csv as below.
+# with open(booksPath, 'r') as csvfile:
+# ...
